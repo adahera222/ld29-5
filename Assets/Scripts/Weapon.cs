b9cba80f63d608feb8872793e8f4ad59802b7f9b
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-14 13:35:55 +1030

    add basic weapon script

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float BulletSpeed;
+    public float Cooldown;
+
+    public void Fire(Vector2 direction)
+    {
+        var bullet = (GameObject)Object.Instantiate(this.Bullet, this.transform.position, Quaternion.identity);
+        bullet.rigidbody2D.velocity = direction * this.BulletSpeed;
+    }
+}
