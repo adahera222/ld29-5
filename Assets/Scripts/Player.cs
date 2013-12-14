f38402d574e1ac15f86d07c113fc1364a1698241
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-14 13:14:28 +1030

    handle basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,33 @@
-﻿using UnityEngine;
+﻿using Annotations;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float Velocity;
+    public Weapon EquippedWeapon;
+
+    [UsedImplicitly] private void Update()
+    {
+        // Handle movement
+        var v = new Vector2();
+        if (Input.GetKey("w")) {
+            v += Vector2.up * this.Velocity;
+        }
+        if (Input.GetKey("s")) {
+            v += -Vector2.up * this.Velocity;
+        }
+        if (Input.GetKey("a")) {
+            v += -Vector2.right * this.Velocity;
+        }
+        if (Input.GetKey("d")) {
+            v += Vector2.right * this.Velocity;
+        }
+
+        this.rigidbody2D.velocity = v;
+
+        // Handle shooting
+        if (Input.GetKey("space")) {
+            this.EquippedWeapon.Fire(v.normalized);
+        }
+    }
 }
