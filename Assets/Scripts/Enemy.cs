1c8b9726fbbd3886609fc911afcdd1f93f99f772
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-14 15:23:59 +1030

    destroy enemy when hit by bullet or player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using Annotations;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [UsedImplicitly] private void OnCollisionEnter2D()
+    {
+        Object.Destroy(this.gameObject);
+    }
+
+    [UsedImplicitly] private void OnTriggerEnter2D()
+    {
+        Object.Destroy(this.gameObject);
+    }
+}
