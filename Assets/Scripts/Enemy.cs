dae0fa0b3b9c0208f141033fbcfea71f73935898
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-14 16:27:46 +1030

    basic enemy ai

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Player player;
+
+    [UsedImplicitly] private void Start()
+    {
+        this.player = Object.FindObjectOfType<Player>();
+    }
+
+    [UsedImplicitly] private void Update()
+    {
+        var target = Vector3.Lerp(this.transform.position, this.player.transform.position, Time.deltaTime);
+        this.transform.position = target;
+    }
+
     [UsedImplicitly] private void OnCollisionEnter2D()
     {
         Object.Destroy(this.gameObject);
