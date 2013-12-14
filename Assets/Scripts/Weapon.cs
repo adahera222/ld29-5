166e55e04f99e2b1ab6042137da9e7b2ecd69b5a
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-14 14:11:05 +1030

    add weapon cooldown

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,10 +5,16 @@ public class Weapon : MonoBehaviour
     public GameObject Bullet;
     public float BulletSpeed;
     public float Cooldown;
+    private float lastFired;
 
     public void Fire(Vector2 direction)
     {
-        var bullet = (GameObject)Object.Instantiate(this.Bullet, this.transform.position, Quaternion.identity);
-        bullet.rigidbody2D.velocity = direction * this.BulletSpeed;
+        if (this.lastFired > this.Cooldown) {
+            var bullet = (GameObject) Object.Instantiate(this.Bullet, this.transform.position, Quaternion.identity);
+            bullet.rigidbody2D.velocity = direction * this.BulletSpeed;
+            this.lastFired = 0;
+        }
+
+        this.lastFired += Time.deltaTime;
     }
 }
