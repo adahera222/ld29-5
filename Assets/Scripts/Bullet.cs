739875b476fff5581d1169b0a305d2cc3651023a
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-14 13:50:45 +1030

    destroy bullets on collision

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,10 @@
+﻿using Annotations;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [UsedImplicitly] private void OnTriggerEnter2D()
+    {
+        Object.Destroy(this.gameObject);
+    }
+}
