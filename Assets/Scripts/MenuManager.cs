914b2f3e90a2b148e733c108facc294a777e111f
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-16 04:03:34 +1030

    add basic menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using Annotations;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public int GameScene;
+
+    [UsedImplicitly] private void Update()
+    {
+        if (Input.GetKeyDown("return")) {
+            Application.LoadLevel(this.GameScene);
+        }
+    }
+}
