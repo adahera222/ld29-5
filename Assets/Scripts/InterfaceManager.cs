757e4fc7cd1d8fda13f816cf99eb527ec21f6e46
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-16 03:46:16 +1030

    add bomb ui element

--- Assets/Scripts/InterfaceManager.cs
+++ Assets/Scripts/InterfaceManager.cs
@@ -6,8 +6,12 @@ public class InterfaceManager : MonoBehaviour
     public GameManager Manager;
     public tk2dTextMesh PointsMesh;
 
+    public Player Player;
+    public tk2dTextMesh BombMesh;
+
     [UsedImplicitly] private void LateUpdate()
     {
         this.PointsMesh.text = "X " + Manager.Points;
+        this.BombMesh.text = "X " + (this.Player.Bomb ? 1 : 0);
     }
 }
