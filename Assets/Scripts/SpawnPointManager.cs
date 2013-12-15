e76b521dfcc390e3228030855314ed641bc3271c
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-15 17:34:17 +1030

    implement spawn point frequency curve

--- Assets/Scripts/SpawnPointManager.cs
+++ Assets/Scripts/SpawnPointManager.cs
@@ -1,24 +1,82 @@
-﻿using System.Collections.Generic;
-using Annotations;
+﻿using Annotations;
 using UnityEngine;
 
 public class SpawnPointManager : MonoBehaviour
 {
+    // Spawn point prefabs in ascending difficulty
     public GameObject[] SpawnPoints;
-    public GameObject Enemies;
 
-    public float Cooldown;
-    private float lastSpawned;
+    // The current level of difficulty
+    public int Level;
+
+    // The maximum difficulty level
+    public int MaxLevel;
+
+    // Seed calculated from current level
+    private float seed;
+
+    private void CalculateDifficulty()
+    {
+        this.seed = (float)this.Level / this.MaxLevel;
+    }
+
+    /*
+     * Spawn frequency
+     */
+
+    // The curve that determines the rate at which spawn points are created
+    public AnimationCurve SpawnPointFrequencyCurve;
+
+    // The variance in spawn frequency (percentage 0..1)
+    public AnimationCurve SpawnPointFrequencyVarianceCurve;
+
+    // The actual spawn frequency (before variance) in seconds
+    private float spawnFrequency;
+
+    // The time elapsed since a spawn point was created
+    private float elapsed;
+
+    // Calculate the frequency at which spawn points are created
+    public void CalculateSpawnFrequency()
+    {
+        var freq = this.SpawnPointFrequencyCurve.Evaluate(this.seed);
+        var variance = this.SpawnPointFrequencyVarianceCurve.Evaluate(this.seed);
+        this.spawnFrequency = Random.Range(freq - variance, freq + variance);
+        Debug.Log("frequency: " + freq);
+        Debug.Log("variance: " + variance);
+        Debug.Log("result: " + this.spawnFrequency);
+    }
+
+    /*
+     * Spawn amounts
+     */
+
+    /*
+     * Spawn proximity
+     */
+
+    /*
+     * Enemy amounts
+     */
+
+    /*
+     * Enemy types
+     */
+
+    [UsedImplicitly] private void Start()
+    {
+        this.CalculateDifficulty();
+        this.CalculateSpawnFrequency();
+    }
 
     [UsedImplicitly] private void Update()
     {
-        if (this.lastSpawned > this.Cooldown) {
-            var rand = Random.Range(1, this.SpawnPoints.Length) - 1;
-            this.CreateSpawnPoint(this.SpawnPoints[rand]);
-            this.lastSpawned = 0;
+        if (this.elapsed > this.spawnFrequency) {
+            this.CreateSpawnPoint(this.SpawnPoints[2]);
+            this.CalculateSpawnFrequency();
+            this.elapsed = 0;
         }
-
-        this.lastSpawned += Time.deltaTime;
+        this.elapsed += Time.deltaTime;
     }
 
     private void CreateSpawnPoint(GameObject spawnPoint)
