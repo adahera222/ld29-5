6cc90958aae3cffa8d6d12b1752c0fb5e83cfcad
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-15 12:15:09 +1030

    rotate line enemies in direction of player

--- Assets/Scripts/Enemies/LineEnemy.cs
+++ Assets/Scripts/Enemies/LineEnemy.cs
@@ -8,7 +8,15 @@ public class LineEnemy : Enemy
     {
         base.Move();
         this.rigidbody2D.velocity = new Vector2();
+        this.rigidbody2D.angularVelocity = 0;
+
         var d = this.Player.transform.position - this.transform.position;
         this.transform.position += (d.normalized * this.Velocity) * Time.deltaTime;
+
+        var a = Quaternion.LookRotation(d);
+        a.x = 0;
+        a.y = 0;
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, a, Time.deltaTime);
     }
 }
