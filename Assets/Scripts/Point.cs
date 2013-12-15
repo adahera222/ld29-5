155dc48d8756069f769983f0ad4a7cc0a27394ac
Lachlan Charlick <lachlan.charlick@gmail.com>
2013-12-16 00:30:34 +1030

    fade and destroy points after 10 seconds

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -1,15 +1,36 @@
-﻿using Annotations;
+﻿using System;
+using Annotations;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Point : MonoBehaviour
 {
     public float Velocity;
     public float Variance;
 
+    public float Lifetime;
+    private float age;
+
     [UsedImplicitly] private void Start()
     {
         this.transform.parent = GameObject.Find("Points").transform;
         this.MoveRandomly();
+    }
+
+    [UsedImplicitly] private void Update()
+    {
+        if (this.age > this.Lifetime) {
+            GameObject.Destroy(this.gameObject);
+        }
+        else {
+            var t = this.age / this.Lifetime;
+            var sprite = (SpriteRenderer)this.renderer;
+            var color = sprite.color;
+            color.a = Mathf.Lerp(1.0f, 0.0f, t);
+            sprite.color = color;
+        }
+
+        this.age += Time.deltaTime;
     }
 
     private void MoveRandomly()
